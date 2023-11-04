40f59bae5ea5d43d634471c914c6d6d9416654c5
PH Saurav <phsaurav29@gmail.com>
2023-11-05 02:56:45 +0600

    feat: Updated pylint args

--- accounts/api/v1/accounts.py
+++ accounts/api/v1/accounts.py
@@ -1,23 +1,29 @@
-from accounts import schemas, models
-from sqlalchemy.orm import Session
+"""Accounts related API Routes"""
 from fastapi import APIRouter, HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
 from core import database, token
+
+from accounts import schemas, models
 from accounts.api.v1 import service
 from accounts.utils import Hash
 
 router = APIRouter(prefix="/api/accounts", tags=["Users"])
+
 
 get_db = database.get_db
 
 
 @router.post("/", response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    """Create a new user"""
     return service.create(request, db)
 
 
 @router.get("/{id}", response_model=schemas.ShowUser)
 def get_user(id: int, db: Session = Depends(get_db)):
+    """Get a particular user"""
     return service.show(id, db)
 
 
@@ -26,6 +32,7 @@ def login(
     request: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(database.get_db),
 ):
+    """Login Authorization"""
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(
