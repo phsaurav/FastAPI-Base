48cbcf80cf505b810b87877083e16f57b8fc1092
PH Saurav <phsaurav29@gmail.com>
2025-05-12 07:38:24 +0600

    feat: Weather App Implemented

--- core/constants.py
+++ core/constants.py
@@ -1,5 +1,12 @@
+import os
+
+
 class Constants:
-    SQLALCHAMY_DATABASE_URL = "sqlite:///./blog.db"
-    SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    """
+    Global Constants
+    """
+
+    weather_api_key = os.environ.get("WEATHER_API_KEY")
+    weather_api_url = "https://api.openweathermap.org/data/2.5/weather"
+    weather_api_units = "metric"
+    api_timeout = 10
